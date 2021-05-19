cf98823b8b077ef6981820d86f818492b078cdba
Janje12 <59922311+Janje12@users.noreply.github.com>
2021-05-19 19:28:26 +0200

    new version

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(logger('dev'));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
